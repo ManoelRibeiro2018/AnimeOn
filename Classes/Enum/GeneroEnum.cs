9bf3d1dc3aa511b638e0dcb264a77ee4abf22e98
MAnoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-02 23:09:40 -0300

    Criando Enum para genero das series

---
+++ Classes/Enum/GeneroEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie.Classes.Enum
+{
+    public enum GeneroEnum
+    {
+        Acao,
+        Aventura,
+        Comedia,
+        Documentario,
+        Drama,
+        Espionagem,
+        Faroeste,
+        Fantasia,
+        FiccaoCientifica,
+        Musical,
+        Romace,
+        Suspence,
+        Terror
+
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie.Classes
+{
+    class Serie : EntidadeBase
+    {
+        public GeneroEnum Genero { get; set; }
+    }
+}
