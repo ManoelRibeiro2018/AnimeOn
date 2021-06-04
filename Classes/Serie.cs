878da3eece5ef5c487b26456af76e4bd44dc8fb3
MAnoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-03 23:29:48 -0300

    Criando interface generica

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serie.Classes.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,22 @@ namespace Serie.Classes
 {
     class Serie : EntidadeBase
     {
-        public GeneroEnum Genero { get; set; }
+        private GeneroEnum Genero { get; set; }
+        private int Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Serie(int id, GeneroEnum genero, int titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
---
+++ Interface/IRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie.Interface
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornadoPorId(int id);
+        void Inserir(T entidade);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
